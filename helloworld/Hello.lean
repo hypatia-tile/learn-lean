2efa754c5cbfd8902195d88af6cd8d6166a85f0b
shinokun <hypatia.tile02021@gmail.com>
2026-05-02 20:20:04 +0900

    2026-05-02

--- helloworld/Hello.lean
+++ helloworld/Hello.lean
@@ -19,3 +19,39 @@ def main : IO Unit := IO.println "Hello, World!"
 #eval if 3 == 4 then "equal" else "not equal"
 
 #eval (1 - 2: Nat)
+
+#check_failure String.append ["hello", " "] world
+
+def maximum (n: Nat) (k : Nat) : Nat :=
+  if n < k then
+    k
+    else n
+
+def add1 (n: Nat): Nat :=  n + 1
+example : Nat -> Nat := add1
+#check maximum 2
+
+def joinStringsWith connective fst sec := String.append fst (String.append connective sec)
+
+#eval joinStringsWith ", " "one" "two"
+#check joinStringsWith 
+
+def volume (height: Nat) width depth := height * width * depth
+
+#eval volume 12 2 3
+
+def Str : Type := String
+
+def aStr : Str := "This is a string"
+
+set_option trace.Meta.synthInstance true
+def NaturalNumber : Type := Nat
+
+λ α ↦ α + 1
+
+
+
+
+
+
+
